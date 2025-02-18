50a77628161d3a09aabef46565e8ac1d19e867e5
maxmanoel <max329900@gmail.com>
2025-02-17 22:09:14 -0300

    alteracao

---
+++ exe01_maxmanoel.py
@@ -0,0 +1,23 @@
+#01 - Escreva um programa que forneça um tipo de aplicação financeira adequada a um investidor a partir de 2 dados fornecidos: 
+# O grau de aceitação de risco e o valor a ser investido. O grau de aceitação de risco deve ser lido no teclado na forma
+#de BX(baixo risco) ou AL(alto risco) se for fornecido algo diferente disto o programa deve mostrar uma mensagem indicando
+#que foi fornecido dado inválido. Para o valor ser deve ser um número real.
+
+n1 = float(input("Digite o valor que sera investido : "))
+n2 = float(input("Digite o grau de risco que voce quer : "))
+soma = valor <1000.00 and valor >= 1000.00
+if risco != "BX" and risco != "AL":
+        print("Dado inválido para o grau de risco.")
+else:
+     if valor < 1000.00:
+        if risco == "BX":
+         print("Aplicação recomendada: Poupança (baixo risco).")
+        else:
+            print("Aplicação recomendada: Renda fixa (alto risco).")
+        else:
+        if risco == "BX":
+            print("Aplicação recomendada: CDB (baixo risco).")
+else:
+            print("Aplicação recomendada: Ações (alto risco).")
+            
+ 
